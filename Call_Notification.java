a3b7380aa0527030a0cbd65052c6f53952741b3a
shashank bhushan <shaanky93@gmail.com>
2014-04-07 04:18:38 +0530

    Work left updated

--- Call_Notification.java
+++ Call_Notification.java
@@ -45,7 +45,7 @@ public class Call_Notification extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel1.setText("   You Have Missed Calls form");
